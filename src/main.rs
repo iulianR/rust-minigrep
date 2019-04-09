f800de7473717002354de8db177fcb5dcc440e88
Iulian Gabriel Radu <iulian.radu67@gmail.com>
2019-04-09 21:44:01 +0300

    Add minigrep

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use std::env;
+use std::process;
+
+use minigrep;
+use minigrep::Config;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
